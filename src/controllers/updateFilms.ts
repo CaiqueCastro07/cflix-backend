c538a1f7c9279e7ac3521957eaf260d876c5c76b
Caique <caiquecastroaraujo@gmail.com>
2022-08-06 16:39:21 -0300

    permitirá apenas um refresh manual a cada 10 minutos

--- src/controllers/updateFilms.ts
+++ src/controllers/updateFilms.ts
@@ -3,17 +3,37 @@ import { response } from '../helpers/helpers'
 //@ts-ignore
 import { delay } from '../helpers/helpers'
 import { } from '../dto/objects/ObjectTypes'
-import { getFilmsInDatabase } from '../database/databaseServices'
 import logger from "../../config/winston"
 import { updateFilmsCatalogue } from '../helpers/helpers'
+import { CronJob } from 'cron'
+
+let interval = false
 
 const updateFilms = async (req: Request, res: Response): Promise<Response> => {
+
+    if(interval) return response(200, "Catalogo atualizado com sucesso há pouco tempo.", false, {}, res);
 
     const updateCatalogueResult = await updateFilmsCatalogue()
 
     if(!updateCatalogueResult) return response(400, "Erro ao atualizar o catalogo de filmes.", true, {}, res)
 
+    interval = true;
+
     return response(200, "Catalogo atualizado com sucesso.", false, {}, res);
 }
+
+const resetInterval = new CronJob(
+    '*/14 * * * *',
+        function(){
+            logger.info("Pertimindo um refresh manual do catalogo de filmes do banco de dados ")
+            interval = false
+        }
+    ,
+    null,
+    true,
+    'America/Sao_Paulo'
+);
+
+resetInterval.start()
 
 export default updateFilms
